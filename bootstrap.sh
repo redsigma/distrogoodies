59ce538de0ba1a1ca8cdf52956a4e5695a6e1840
RedSigma <avanganttarix@gmail.com>
2026-05-29 17:53:54 +0300

    allow private template files

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ if [ ! -d "$SCRIPT_DIR/private_data" ]; then
     echo
 
     git clone --branch main https://${GIT_TOKEN}@github.com/redsigma/distrogoodies_internal.git $SCRIPT_DIR/private_data
+    rsync -aHAX --numeric-ids "${SCRIPT_DIR}/private_data/templates" "${SCRIPT_DIR}/"
 fi
 
 
